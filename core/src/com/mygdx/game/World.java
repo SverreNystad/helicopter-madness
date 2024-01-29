ab6c3cbf8bad9e3573080f61855d3c52371e71b0
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-01-29 16:42:21 +0100

    Feat: Add font rendering for helicopter position

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -9,12 +10,16 @@ public class World {
     private SpriteBatch batch;
     private float width, height;
     private Array<Helicopter> helicopters;
+    private BitmapFont font;
+   
 
     public World() {
         batch = new SpriteBatch();
         helicopters = new Array<Helicopter>();
         this.width = Gdx.graphics.getWidth();
         this.height = Gdx.graphics.getHeight();
+
+        font = new BitmapFont();
     }
 
     public void addHelicopter(Helicopter helicopter) {
@@ -30,10 +35,16 @@ public class World {
     public void render() {
         update(Gdx.graphics.getDeltaTime());
         ScreenUtils.clear(1, 0, 0, 1);
-
+        batch.begin();
         for (Helicopter helicopter : helicopters) {
             helicopter.render(batch);
+    
+            String positionText = "Position: (" + helicopter.getX() + ", " + helicopter.getY() + ")";
+            // Drawing text at top-left corner
+            font.draw(batch, positionText, 10, Gdx.graphics.getHeight() - 10); 
         }
+        batch.end();
+   
     }
 
     public float getWidth() {
