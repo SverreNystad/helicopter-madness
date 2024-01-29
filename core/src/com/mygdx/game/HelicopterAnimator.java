ff6cddd4ef5f7cc56f240460e2b0d0b095e2d852
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-01-29 17:44:34 +0100

    Feat: Add HelicopterAnimator class for sprite animation

---
+++ core/src/com/mygdx/game/HelicopterAnimator.java
@@ -0,0 +1,56 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Array;
+
+public class HelicopterAnimator {
+
+    private Array<Texture> sprites;
+    private float displayTime;
+    private boolean shallLoop;
+    private float currentTime = 0;
+
+    private static final String imagePath1 = "heli1.png";
+    private static final String imagePath2 = "heli2.png";
+    private static final String imagePath3 = "heli3.png";
+    private static final String imagePath4 = "heli4.png";
+
+    /**
+     * Creates a new HelicopterAnimator.
+     * @param displayTime in seconds
+     * @param shallLoop whether the animation shall loop or not
+     */
+    public HelicopterAnimator(float displayTime, boolean shallLoop) {
+        this.displayTime = displayTime;
+        this.shallLoop = shallLoop;
+        this.sprites = new Array<Texture>();
+        this.sprites.add(new Texture(imagePath1));
+        this.sprites.add(new Texture(imagePath2));
+        this.sprites.add(new Texture(imagePath3));
+        this.sprites.add(new Texture(imagePath4));
+    }
+
+    /**
+     * Updates the animations clock.
+     * @param deltaTime in seconds
+     */
+    public void update(float deltaTime) {
+        currentTime += deltaTime;
+    }
+
+    
+    public Texture getSprite() {
+        int index = (int) (currentTime / displayTime);
+        if (index >= sprites.size) {
+            if (shallLoop) {
+                index = index % sprites.size;
+            } else {
+                index = sprites.size - 1;
+            }
+        }
+        return sprites.get(index);
+    }
+
+    
+
+}
