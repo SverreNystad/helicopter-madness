697bc3e483bb106378925ccfe56733607a91f11d
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-02-14 00:06:46 +0100

    Docs: Add documentation for the World class

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -2,9 +2,17 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.utils.Array;
 
+/**
+ * Represents the world in the game, containing helicopters and managing their interactions.
+ * This class is responsible for updating the state of the world, including moving helicopters
+ * and handling their collisions. It uses a singleton pattern to ensure only one instance of the world exists.
+ * To read more about the singleton pattern, see https://en.wikipedia.org/wiki/Singleton_pattern
+ */
 public class World {
     private float width, height;
+    /** A collection of helicopters currently in the world. */
     private Array<Helicopter> helicopters;
+    /** The animator responsible for rendering the world and its entities. */
     private WorldAnimator animator;
 
     private static World instance = new World();
@@ -21,10 +29,21 @@ public class World {
         return instance;
     }
 
+    /**
+     * Returns the singleton instance of the World.
+     *
+     * @return The singleton instance of the World.
+     */
     public void addHelicopter(Helicopter helicopter) {
         helicopters.add(helicopter);
     }
 
+    /**
+     * Updates the state of the world and all its entities based on the given delta time.
+     * This includes moving helicopters and checking for collisions between them.
+     *
+     * @param deltaTime The time in seconds since the last update.
+     */
     public void update(float deltaTime) {
         for (Helicopter helicopter : helicopters) {
             helicopter.update(deltaTime);
@@ -41,11 +60,20 @@ public class World {
         }
     }
 
+    /**
+     * Handles the collision between two helicopters by simulating a bounce effect.
+     *
+     * @param h1 The first helicopter involved in the collision.
+     * @param h2 The second helicopter involved in the collision.
+     */
     private void handleCollision(Helicopter h1, Helicopter h2) {
         // Reverse the velocities of the helicopters to simulate a bounce
         h1.handleCrash(h2);
     }
 
+    /**
+     * Renders the world and its helicopters.
+     */
     public void render() {
         animator.render(helicopters);
     }
